2aa1e4fd1dfe93326e85bf2dbdc964efe42bbddd
Anton.Balaniuc <anton.balaniuc@gmail.com>
2017-11-07 21:46:25 +0000

    add comments

--- iostreams/src/main/java/org/apache/logging/log4j/samples/iostreams/App.java
+++ iostreams/src/main/java/org/apache/logging/log4j/samples/iostreams/App.java
@@ -7,10 +7,12 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("This message should appear on console");
+        // redirect system.Out to a file
         System.setOut(IoBuilder.forLogger(LogManager.getLogger("system.out")).buildPrintStream());
         System.out.println("But this message should be redirected to a file");
 
         System.err.println("This error should appear on console");
+        // redirect system.Err to a file
         System.setErr(IoBuilder.forLogger(LogManager.getLogger("system.err")).buildPrintStream());
         System.err.println("But this error should be redirected to a file");
     }
